4ef329a465cc09b571825c3e7a513d17ec250388
Sean P. Goggins <s@goggins.com>
2018-06-18 10:41:17 -0500

    filtered NAs, working on components

--- R/qual-filter.R
+++ R/qual-filter.R
@@ -19,30 +19,30 @@ loadSummaryData<-function() {
   bind_rows(lapply(list.files(path = path,pattern = "*_nodedesc.csv"),function(x) read.csv(paste(path,x,sep=""))))
 }
 
-CTCIndex <- loadSummaryData()
+## Description of data elements in the node description component summaries
+#  level - From "main.R" : Note that this function decorates nodes with a "level" property, corresponding to the day. This is used in the sugiyama plots below
+#  comp - the component to be extracted
+#  post_id_native : mapped from "name" in component file to avoid keyword conflicts. More descriptive
+#  post_timestamp : mapped from "time" in the component file to avoid keyword conflicts. More descriptive
+#  pid: individual poster ID. I think this may be introduced by earlier code.  Not native.  Maps to a "poster"
+#  poster: person making the post
 
-class(CTCIndex$time) = c('POSIXt','POSIXct')
+#  csize - Number of posts in the component
+#  triangles - 
 
-# CTCLonger <- subset(CTCIndex, CTCIndex$maxpth > 3)
+loadPostingData<-function(corpus) {
+  require(RMySQL)
+  con<-dbConnect(MySQL(),user=dbuser,password=dbpassword,dbname=dbname,host=host)
+  rs<-dbSendQuery(con,paste("select qid, localID, date, poster from ",corpus,sep=""))
+  data<-fetch(rs,n=-1)
+  dbDisconnect(con)
+  return(data)
+}
 
-hist(CTCIndex$maxpth)
-
-CTCIndex$comp <- as.factor(CTCIndex$comp)
-CTCIndex$topic <- as.factor(CTCIndex$topic)
-
-colnames(CTCIndex)[2] <- "post_id_native"
-colnames(CTCIndex)[3] <- "post_timestamp"
-
-
-# qualPlotPathDescription<-function() {
-#   #x<-read.csv("component_summary.csv")
-#   x <- CTCLonger
-#   x4 <- x %>% filter(csize>1) %>% group_by(corpus,comp,topic) %>% summarize(ind=mean(indegree[indegree>0]),outd=mean(outdegree[outdegree>0]),maxp=mean(maxpth[outdegree==0]),cs=max(csize)) 
-#   str(x4)
-#   ggplot(x4,res=600, height = 12, width = 12, aes(x=ind,y=outd,color=maxp))+scale_color_viridis(name="Mean Path\nLength",direction = -1)+scale_x_log10()+scale_y_log10()+geom_jitter(size=.7)+theme_dark()+ylab("Mean Out Degree")+xlab("Mean In Degree")+geom_label_repel(aes(label=paste(comp), fill=factor(topic)), color='white', size=3.5)+
-#     theme(legend.position = "bottom")
-#   
-# }
+dbname = "webmd"
+dbuser = "webmd"
+dbpassword = "pickle"
+host = "augurlabs.io"
 
 qualPlotPathDescription<-function(CTCNow) {
   #x<-read.csv("component_summary.csv")
@@ -54,20 +54,57 @@ qualPlotPathDescription<-function(CTCNow) {
   
 }
 
+CTCIndex <- loadSummaryData()
+
+class(CTCIndex$time) = c('POSIXt','POSIXct')CT
+
+# hist(CTCIndex$maxpth)
+
+CTCIndex$comp <- as.factor(CTCIndex$comp)
+CTCIndex$topic <- as.factor(CTCIndex$topic)
+
+colnames(CTCIndex)[2] <- "post_id_native"
+colnames(CTCIndex)[3] <- "post_timestamp"
+
+
+## Removing NAs from indegree and out degree, as these seem to get introduced for every forum even a person is not in it
+# How many rows have NA's
+row.has.na <- apply(CTCIndex, 1, function(x){any(is.na(x))})
+sum(row.has.na)
+
+no.na <- apply(CTCIndex, 1, function(x){all(!is.na(x))})
+sum(no.na)
+
+nrow(CTCIndex)
+
+CTCIndex.filtered <- CTCIndex[!row.has.na,]
+
+nrow(CTCIndex)
+nrow(CTCIndex.filtered)
+
+# Somewhere along the line a row is introduced for every user in every forum. 
+# This is indicated in the data with NA's in the in degree and outdegree, and the existance of forum information for users in every component.
+# Removing any row with an NA cleans this up
+
+
+### 
+
 # qualPlotPathDescription()
 
-
-# p + geom_label_repel(aes(label = rownames(df),
-#                          fill = factor(cyl)), color = 'white',
-#                      size = 3.5) +
-#   theme(legend.position = "bottom")
-
-#my.data.frame <- data[(data$V1 > 2) & (data$V2 < 4), ]
-
-
-CTCIndexADHD <- CTCIndex[(CTCIndex$comp==608) & CTCIndex$corpus=="anxiety_and_panic_disorders_exchange",]
+CTCIndexADHD <- CTCIndex.filtered[(CTCIndex.filtered$comp==608) & CTCIndex.filtered$corpus=="anxiety_and_panic_disorders_exchange",]
 
 CTCIADHDSorted <-CTCIndexADHD[order(CTCIndexADHD$post_timestamp, CTCIndexADHD$post_id_native),]
 
+### Experimental
 
+###
 
+CTCIndex_Ordered <- CTCIndex.filtered[order(CTCIndex.filtered$comp, CTCIndex.filtered$corpus, 
+                                            CTCIndex.filtered$maxpth, CTCIndex.filtered$csize, decreasing=TRUE),]
+
+CTCIndex_Ordered <- CTCIndex.filtered[order(CTCIndex.filtered$csize, CTCIndex.filtered$comp, CTCIndex.filtered$corpus, 
+                                            CTCIndex.filtered$maxpth, CTCIndex.filtered$csize, decreasing=TRUE),]
+
+nrow(CTCIndex_Ordered[(CTCIndex_Ordered$comp==13),])
+
+component13 <- CTCIndex_Ordered[(CTCIndex_Ordered$comp==13),]
